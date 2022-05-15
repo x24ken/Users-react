b2f9e97b9de241a3c3a5e5b623636a5288f33d62
x24ken <x24ken@gmail.com>
2022-05-15 18:22:42 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
 import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "にしけん",
-  image: "https://source.unsplash.com/NE0XGVKTmcA",
-  mail: "12345@example.com",
-  phone: "000-0000-9999",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://google.com"
-};
-
 export default function App() {
   return <Router />;
 }
